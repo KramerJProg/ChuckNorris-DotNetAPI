b269026840170c24a80097a9b9a4df0a023b5ee9
Kramer Jensen <Kramerjensend@gmail.com>
2021-04-25 18:49:20 -0700

    Added documentation to the Btn and formLoad event

--- ChuckNorrisForm/Form1.cs
+++ ChuckNorrisForm/Form1.cs
@@ -18,12 +18,24 @@ namespace ChuckNorrisForm
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Clicking this button will generate a random joke from the 
+        /// ChuckNorrisAPI.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void JokeBtn_Click(object sender, EventArgs e)
         {
             Joke joke = await ChuckNorrisClient.GetRandomJoke();
             MessageBox.Show(joke.JokeText);
         }
 
+        /// <summary>
+        /// Loads the form [Design] and gets the categories (Explicit OR Nerdy) from the
+        /// ChuckNorrisAPI.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void Form1_Load(object sender, EventArgs e)
         {
             IEnumerable<string> categories = await ChuckNorrisClient.GetCategories();
